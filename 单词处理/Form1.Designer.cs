d7eff8ca86aa8a3919193cab19824bbe1cc0f598
Hansi7 <hansi-go@163.com>
2014-10-29 10:33:09 +0800

    Release V1 fisrt Commit

---
+++ 单词处理/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace 单词处理
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txt_result = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 13);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "浏览...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(654, 13);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(778, 10);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "获得音标";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(654, 47);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(116, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(654, 81);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(116, 26);
+            this.textBox3.TabIndex = 4;
+            // 
+            // txt_result
+            // 
+            this.txt_result.Location = new System.Drawing.Point(14, 142);
+            this.txt_result.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_result.Multiline = true;
+            this.txt_result.Name = "txt_result";
+            this.txt_result.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_result.Size = new System.Drawing.Size(833, 421);
+            this.txt_result.TabIndex = 4;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(14, 50);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(634, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(107, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(14, 83);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(75, 20);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "搜索音标";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 576);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.txt_result);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Lucida Sans Unicode", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txt_result;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ 单词处理/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 单词处理
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            ofd.Multiselect = false;
+
+            if (ofd.ShowDialog()!= System.Windows.Forms.DialogResult.OK)
+            {
+                return;
+            }
+
+            string fi = ofd.FileName;
+            this.label1.Text = fi;
+            this.Update();
+            var s1 = System.IO.File.ReadAllLines(fi);
+            string[] result = new string[s1.Count()];
+
+            for (int i = 0; i < s1.Count(); i++)
+            {
+                result[i] = s1[i].TrimStart(' ');
+            }
+            //两个意思的单词
+            System.Text.RegularExpressions.Regex r = new System.Text.RegularExpressions.Regex(@"(\w+)\s+((n|adj|vt|adv|vi|prep|abbr)\.\s*\&*\s*(n|adj|vt|adv|vi|prep|abbr)\.)\s*(.+)");
+            //一个意思的单词
+            System.Text.RegularExpressions.Regex r2 = new System.Text.RegularExpressions.Regex(@"(\w+)\s+((n|adj|vt|adv|vi|prep|abbr)\.)\s*(.+)");
+            //短语
+            System.Text.RegularExpressions.Regex r3 = new System.Text.RegularExpressions.Regex(@"((\w+\s+)+)([^a-zA-Z()]+\w+)");
+            //续上一个单词的解释
+            System.Text.RegularExpressions.Regex r4 = new System.Text.RegularExpressions.Regex(@"^((n|adj|vt|adv|vi|prep|abbr)\.).*?(?=[（\(|\u4e00-\u9fa5])");
+
+            System.Text.RegularExpressions.Regex rWord = new System.Text.RegularExpressions.Regex(@"[a-zA-Z]+");
+
+            StringBuilder sb = new StringBuilder();
+            StringBuilder sb2 = new StringBuilder();
+            StringBuilder sb3 = new StringBuilder();
+            StringBuilder sb4 = new StringBuilder();
+            this.progressBar1.Maximum = s1.Count();
+            for (int i = 0; i < s1.Count(); i++)
+            {
+                if (r.IsMatch(result[i]))
+                {
+                    var m = r.Match(result[i]);
+                    sb.Append("1\t" + m.Groups[1] + "\t" + m.Groups[2] + "\t" +  m.Groups[5]);
+                    if (rWord.IsMatch(m.Groups[1].ToString())&&checkBox1.Checked)
+                    {
+                        sb.AppendLine("\t" + GetPhonetic(m.Groups[1].ToString(), false));
+                    }
+                    else
+                    {
+                        sb.AppendLine();
+                    }
+                }
+                else if (r2.IsMatch(result[i]))
+                {
+                    var m2 = r2.Match(result[i]);
+                    sb.Append("2\t" + m2.Groups[1] + "\t" + m2.Groups[2] + "\t" + m2.Groups[4]);
+                    if (rWord.IsMatch(m2.Groups[1].ToString()) && checkBox1.Checked)
+                    {
+                        sb.AppendLine("\t" + GetPhonetic(m2.Groups[1].ToString(), false));
+                    }
+                    else
+                    {
+                        sb.AppendLine();
+                    }
+                }
+                else if (r3.IsMatch(result[i]))
+                {
+                    var m = r3.Match(result[i]);
+                    sb.AppendLine("3\t" + m.Groups[1].ToString().TrimStart(' ') + "\t\t" + m.Groups[3]);
+                }
+                else if (r4.IsMatch(result[i]))
+                {
+                    var m =r4.Match(result[i]);
+                    sb.AppendLine("4\t\t"  + m.Groups[0] + "\t" +  result[i].Replace(m.Groups[0].ToString(),"").Trim());
+                }
+                else
+                {
+                    sb.AppendLine("5\t" + result[i]);
+                }
+
+                this.progressBar1.Value = i+1;
+                this.txt_result.AppendText(sb.ToString());
+                sb2.Append(sb.ToString());
+                sb.Clear();
+                //if (i==40)
+                //{
+                //    break;
+                //}
+            }
+
+            Save("r1", sb2.ToString());
+            MessageBox.Show("Done!");
+            //Save("r3", sb3.ToString());
+            //Application.Exit();
+
+        }
+
+        private void Save(string fileName, string content)
+        {
+            string f = AppDomain.CurrentDomain.BaseDirectory + fileName + ".txt";
+            System.IO.File.WriteAllText(f, content);
+            System.Diagnostics.Process.Start(f);
+        }
+
+        private string GetPhonetic(string word,bool isUSA)
+        {
+            using (WebClient wc = new WebClient())
+            {
+
+                var bs = wc.DownloadData(new Uri("http://dict.youdao.com/search?le=eng&q="+word+"&keyfrom=dict.top"));
+
+                HtmlAgilityPack.HtmlDocument html = new HtmlAgilityPack.HtmlDocument();
+
+                var s = System.Text.Encoding.UTF8.GetString(bs);
+
+                html.LoadHtml(s);
+                
+                var nodes =  html.DocumentNode.SelectNodes("//div/span['class=phonetic']/span");
+
+                //lucida sans unicode",arial,sans-serif
+                if (nodes.Count >= 2)
+                {
+                    if (isUSA)
+                    {
+                        return nodes[2].InnerText;
+                    }
+                    else
+                    {
+                        return nodes[1].InnerText;
+                    } 
+                }
+                return "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.textBox2.Text = GetPhonetic(this.textBox1.Text, false);
+            this.textBox3.Text = GetPhonetic(this.textBox1.Text, true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
